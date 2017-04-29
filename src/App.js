cdd39de2ecd963e3bb354aee832c21a282a58889
samnags <sam.nagourney@gmail.com>
2017-04-29 12:40:46 -0700

    landing page and home page with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import Landing from './components/landing'
 import Submission from './components/submission'
+
 import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      node: 'landing'
+    }
+}
   submit = (values) => {
     console.log(values)
+  }
+  handleClick = (e) => {
+    e.preventDefault()
+    this.setState({
+      node: 'submission'
+    })
   }
 
   render() {    
     return (
       <div>
-        <Submission onSubmit={this.submit}/>
+        <Landing onClick={this.handleClick}/>
+        {this.state === 'submission' ? <Submission onSubmit={this.submit}/> : null }        
       </div>
     );
   }
