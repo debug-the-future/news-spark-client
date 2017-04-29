075d7b54af6d197ff5a8553d0b23b53b4cf7590e
Matt Casey <matt@mattcasey.nyc>
2017-04-29 14:29:39 -0700

    responsive preview

--- src/components/SitePreview/SitePreview.js
+++ src/components/SitePreview/SitePreview.js
@@ -1,28 +1,49 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import jade from 'jade'
+import jade from 'jade';
+import * as BS from 'react-bootstrap';
 
 import { template } from './template'
 import './SitePreview.css';
 
-export const SitePreview = (props) => {
-  const sitePreview = jade.compile(template);
+export class SitePreview extends React.Component {
+  prototype = {
+    data: PropTypes.shape({
+      headline: PropTypes.string,
+      what: PropTypes.string,
+      asset: PropTypes.string,
+      details: PropTypes.string,
+      hashtag: PropTypes.string,
+    }).isRequired
+  }
 
-  return (
-    <div className="site-preview">
-      <div className="site-preview-container">
-        <div dangerouslySetInnerHTML={{ __html: sitePreview(props.data) }}></div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      previewSize: 'mobile'
+    }
+  }
+
+  setPreviewSize = (size) => {
+    this.setState({ previewSize: size });
+  }
+
+  render() {
+    const sitePreview = jade.compile(template);
+
+    return (
+      <div className="site-preview">
+        <div>
+          <BS.Nav activeKey={this.previewSize} onSelect={this.setPreviewSize} bsStyle="pills">
+            <BS.NavItem eventKey={'mobile'}>mobile</BS.NavItem>
+            <BS.NavItem eventKey={'tablet'}>tablet</BS.NavItem>
+            <BS.NavItem eventKey={'desktop'}>desktop</BS.NavItem>
+          </BS.Nav>
+        </div>
+        <div className={`site-preview-container ${this.state.previewSize}`}>
+          <div dangerouslySetInnerHTML={{ __html: sitePreview(this.props.data) }}></div>
+        </div>
       </div>
-    </div>
-  );
-}
-
-SitePreview.prototype = {
-  data: PropTypes.shape({
-    headline: PropTypes.string,
-    what: PropTypes.string,
-    asset: PropTypes.string,
-    details: PropTypes.string,
-    hashtag: PropTypes.string,
-  }).isRequired
+    );
+  }
 }
