c6df1f057bc3886400d67ac3b59a2767d5de91bd
Matt Casey <matt@mattcasey.nyc>
2017-04-29 13:01:29 -0700

    fix page refresh on submit

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,19 +5,19 @@ import * as BS from 'react-bootstrap'
 
 class Landing extends Component {
     render() {
-        const { handleClick } = this.props;        
+        const { handleSubmit } = this.props;
         return(
             <div>
                 <h1>Let's Begin</h1>
-            <BS.Form onSubmit={handleClick}>            
-                <BS.FormGroup bsSize="large">                                
-                    <Field placeholder="What Happened?" name="what" component="input" type="textarea" rows={10} cols={30}/>
-                </BS.FormGroup>
-                <BS.FormGroup bsSize="large">            
-                    <Field placeholder="Upload or Paste Image/Video" name="asset" component="input" type="text"/>
-                </BS.FormGroup>
-                <BS.Button bsStyle="success" bsSize="large" type="submit">Get Live Site</BS.Button>           
-            </BS.Form>                
+                <BS.Form onSubmit={handleSubmit}>
+                    <BS.FormGroup bsSize="large">
+                        <Field placeholder="What Happened?" name="what" component="input" type="textarea" rows={10} cols={30}/>
+                    </BS.FormGroup>
+                    <BS.FormGroup bsSize="large">
+                        <Field placeholder="Upload or Paste Image/Video" name="asset" component="input" type="text"/>
+                    </BS.FormGroup>
+                    <BS.Button bsStyle="success" bsSize="large" type="submit">Get Live Site</BS.Button>
+                </BS.Form>
             </div>
         )
     }
