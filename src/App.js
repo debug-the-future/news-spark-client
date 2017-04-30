b2fa8cb8d85f3e3de4bf59b9f1cb5031877ff535
Matt Casey <matt@mattcasey.nyc>
2017-04-30 12:35:22 -0700

    default state

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
     super(props)
 
     this.state = {
-      // node: 'landing'
-      node: 'submission'
+      node: 'landing'
+      // node: 'submission'
     }
   }
 
