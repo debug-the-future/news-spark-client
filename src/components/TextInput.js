f92002236a92d30fbf198aae2dab9c2c168eec9a
Matt Casey <matt@mattcasey.nyc>
2017-04-30 11:12:06 -0700

    TextInput component

---
+++ src/components/TextInput.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as BS from 'react-bootstrap';
+
+const TextInput = ({ input }) => (
+  <BS.FormControl {...input} />
+);
+
+export default TextInput;
--- src/components/submission.js
+++ src/components/submission.js
@@ -2,41 +2,43 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import * as BS from 'react-bootstrap'
 
+import TextInput from './TextInput';
+
 class Submission extends Component {
     render() {
         const { handleSubmit } = this.props;
         return(
         <div className="hero-box-submission">
-            <BS.Form onSubmit={handleSubmit}>        
-                <BS.FormGroup>            
+            <BS.Form onSubmit={handleSubmit}>
+                <BS.FormGroup>
                     <label htmlFor="headling">Headline</label>
-                    <Field name="headline" component="input" type="text" placeholder="In 120 words or less"/>            
+                    <Field name="headline" component={TextInput} type="text" placeholder="In 120 words or less"/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="what">What is this all about?</label>
                     <Field name="what" component="input" type="textarea" rows={10} cols={30}/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="asset">URL for Video for Image</label>
                     <Field name="asset" component="input" type="text"/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="details">What else should people know?</label>
                     <Field name="details" component="input" type="text"/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="help">How can people help?</label>
                     <Field name="help" component="input" type="text"/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="hashtag">Do you have a hashtag?</label>
                     <Field name="hashtag" component="input" type="url" placeholder="https://twitter.com/hashtag/fill-me"/>
                 </BS.FormGroup>
-                <BS.FormGroup>            
+                <BS.FormGroup>
                     <label htmlFor="contact">How can people contact you?</label>
                     <Field name="contact" component="input" type="url" placeholder="https://twitter.com/hashtag/fill-me"/>
                 </BS.FormGroup>
-                <BS.Button bsStyle="success" bsSize="large" type="submit">Submit </BS.Button>           
+                <BS.Button bsStyle="success" bsSize="large" type="submit">Submit </BS.Button>
         </BS.Form>
     </div>
     );
