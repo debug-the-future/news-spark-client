209bc782f62bc57262b4a6250fccea6b3cf5af2f
samnags <sam.nagourney@gmail.com>
2017-04-30 13:03:10 -0700

    loading modal

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
+import * as BS from 'react-bootstrap'
 
 const Loading = () => {
-    <div className="loading">
+    <div className="static-modal loading">
+     <Modal.Dialog>
+        <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+     </Modal.Dialog>
+
         <h3>We're building your website now. Hold tight</h3>
         <div className="spinner"></div>
     </div>
